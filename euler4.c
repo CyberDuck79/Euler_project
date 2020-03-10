4e48e1db73947b09d899ef01df2763975ee6089a
Flavien Henrion <flavien.henrion@protonmail.com>
2020-03-10 11:36:41 +0100

    euler 4

--- euler4.c
+++ euler4.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/26 10:29:51 by fhenrion          #+#    #+#             */
-/*   Updated: 2019/12/26 10:38:01 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/03/10 11:36:17 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,62 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-#include <stdio.h>
-
-int		main(void)
+typedef enum	e_result
 {
-	int		number_1 = 100;
-	int		number_2 = 100;
-	char	*string[30];
+	NO,
+	YES
+}				t_result;
 
-	sprintf(string,"%li",number_1 * number_2);
-	
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+static int	is_palindromic(const char *result)
+{
+	size_t	len = strlen(result);
+	size_t	index = 0;
+	size_t	middle;
+
+	if (len == 0)
+		return (NO);
+	middle = len / 2;
+	while (index < middle)
+	{
+		len--;
+		if (result[index] != result[len])
+			return (NO);
+		index++;
+	}
+	return (YES);
+}
+
+int			main(void)
+{
+	long long	numbers[2];
+	char		string[7];
+	long long	results[4];
+
+	bzero(results, sizeof(results));
+	for (numbers[0] = 999; numbers[0] > 99; numbers[0]--)
+	{
+		for (numbers[1] = numbers[0]; numbers[1] > 99; numbers[1]--)
+		{
+			bzero(string, sizeof(string));
+			sprintf(string, "%lli", numbers[0] * numbers[1]);
+			//printf("%lli * %lli = %s\n", numbers[0], numbers[1], string);
+			if (is_palindromic(string) == YES)
+			{
+				if ((results[3] = atoll(string)) > results[2])
+				{
+					results[2] = results[3];
+					results[0] = numbers[0];
+					results[1] = numbers[1];
+				}
+				else if (numbers[0] <= results[0] && numbers[1] >= results[1])
+					bzero(numbers, sizeof(numbers));
+			}
+		}
+	}
+	printf("%lli\n", results[2]);
 	return (0);
 }
