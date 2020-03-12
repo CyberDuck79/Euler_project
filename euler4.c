94d62e30265a39de3066b4f48c46e6e8d248e0d6
Flavien Henrion <flavien.henrion@protonmail.com>
2020-03-12 21:31:21 +0100

    tweak

--- euler4.c
+++ euler4.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/26 10:29:51 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/03/10 11:36:17 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/03/12 21:30:52 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,33 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-typedef enum	e_result
-{
-	NO,
-	YES
-}				t_result;
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static int	is_palindromic(const char *result)
+typedef enum	e_bool
+{
+	false,
+	true
+}				t_bool;
+
+static t_bool	is_palindromic(const char *result)
 {
 	size_t	len = strlen(result);
 	size_t	index = 0;
-	size_t	middle;
+	size_t	middle = len / 2;;
 
-	if (len == 0)
-		return (NO);
-	middle = len / 2;
 	while (index < middle)
 	{
 		len--;
 		if (result[index] != result[len])
-			return (NO);
+			return (false);
 		index++;
 	}
-	return (YES);
+	return (true);
 }
 
-int			main(void)
+int				main(void)
 {
 	long long	numbers[2];
 	char		string[7];
@@ -56,10 +53,8 @@ int			main(void)
 	{
 		for (numbers[1] = numbers[0]; numbers[1] > 99; numbers[1]--)
 		{
-			bzero(string, sizeof(string));
 			sprintf(string, "%lli", numbers[0] * numbers[1]);
-			//printf("%lli * %lli = %s\n", numbers[0], numbers[1], string);
-			if (is_palindromic(string) == YES)
+			if (is_palindromic(string) == true)
 			{
 				if ((results[3] = atoll(string)) > results[2])
 				{
@@ -72,6 +67,6 @@ int			main(void)
 			}
 		}
 	}
-	printf("%lli\n", results[2]);
+	printf("%lli * %lli = %s\n", results[0], results[1], results[2]);
 	return (0);
 }
