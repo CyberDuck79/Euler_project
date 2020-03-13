b0a7745fe0a2db3d5d01c26ddc733077f17a7067
Flavien Henrion <flavien.henrion@protonmail.com>
2020-03-13 15:10:17 +0100

    solution 7

--- euler7.c
+++ euler7.c
@@ -6,7 +6,7 @@
 /*   By: fhenrion <fhenrion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 09:08:37 by fhenrion          #+#    #+#             */
-/*   Updated: 2020/03/13 09:38:30 by fhenrion         ###   ########.fr       */
+/*   Updated: 2020/03/13 15:07:17 by fhenrion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,41 @@ What is the 10 001st prime number?
 #include <math.h>
 #include <stdio.h>
 
+typedef enum	e_bool
+{
+	false,
+	true
+}				t_bool;
+
+int		is_prime(long nb) {
+	long	test;
+
+	for (test = 3; test * test <= nb; test += 2)
+		if (nb % test == 0)
+			return (false);
+	return (true);
+}
+
 int		next_prime()
 {
-	long long			prime = 3;
-	static long long	nb = 1;
+	static long	prime = 3;
 
-	nb += 2;
-	while (prime <= sqrt(nb))
-	{
-		if (!(nb % prime))
-			nb += 2;
+	while (is_prime(prime) == false)
 		prime += 2;
-	}
-	return (nb);
+	prime += 2;
+	return (prime - 2);
 }
 
 int		main(void)
 {
-	int			count = 0;
 	long long	nb;
+	int			count = 1;
 
-	while (count != 19)
+	//printf("%i : %i\n", 1, 2);
+	while (count != 10001)
 	{
 		nb = next_prime();
-		printf("%i : %lli\n", count, nb);
+		//printf("%i : %lli\n", count + 1, nb);
 		count++;
 	}
 	printf("%lli\n", nb);
